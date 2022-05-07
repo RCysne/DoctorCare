87a45e5a4293d12cdcad0f18f263d83b0699fa07
Ronaldo Cysne <ronaldocysne@gmail.com>
2022-05-07 19:19:57 -0300

    Mobile first

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+// Executar as funções que estão no start depois que a página for lida podem ser utilizada outras funções que estão doando conflito quando a página ainda não foi carregada.
+
+window.addEventListener('load', start);
+
+function start() {
+  onScroll();
+}
+
 // ======    Navigation Scroll
 
 function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
 
+function showNavOnScroll() {
   let scroll = document.getElementById('navigation')
   scroll.classList.add('scroll');
   if(scrollY === 0) {
     scroll.classList.remove('scroll');
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  let topButton = document.getElementById('backToTopButton');
+  // console.log(scrollY)
+
+  if(scrollY > 550) {
+    topButton.classList.add('show')
+  } else {
+    topButton.classList.remove('show');
   }
 }
 
@@ -32,3 +55,4 @@ ScrollReveal({
 #about header,
 #about .content
 `);
+
